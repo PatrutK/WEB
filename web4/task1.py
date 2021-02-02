9ff2109b096949a0aa15be63277edc013dda0015
PatrutK <utkin.best.no@gmail.com>
2021-02-02 18:33:34 +0300

    задача 1

--- web4/task1.py
+++ web4/task1.py
@@ -1,7 +1,7 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("arg", nargs='*', default='no args')
+parser.add_argument("arg", nargs='*', default=['no args'])
 
 args = parser.parse_args()
 for arg in args.arg:
